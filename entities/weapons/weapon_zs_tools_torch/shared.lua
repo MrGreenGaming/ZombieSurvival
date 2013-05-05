b4f2d7f20952af6ca9dc1318d7fd2b8c37a6c6e0
Jarno <2751672+JarnoVgr@users.noreply.github.com>
2013-05-05 15:09:34 +0000

    - Improved blowtorch - Optimized blowtorch code

--- entities/weapons/weapon_zs_tools_torch/shared.lua
+++ entities/weapons/weapon_zs_tools_torch/shared.lua
@@ -34,7 +34,7 @@ SWEP.WorldModel = "models/Weapons/w_grenade.mdl"
 
 SWEP.Base				= "weapon_zs_base_dummy"
 
-SWEP.Primary.ClipSize =60
+SWEP.Primary.ClipSize = 60
 SWEP.Primary.DefaultClip = 60
 SWEP.Primary.Automatic = true
 SWEP.Primary.Ammo = "SniperRound"
@@ -54,7 +54,6 @@ SWEP.TempAng = SWEP.IdleAngle
 SWEP.WalkSpeed = 190
 
 function SWEP:InitializeClientsideModels()
-	
 	self.ViewModelBoneMods = {
 		["ValveBiped.Bip01_L_Clavicle"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0, 0), angle = Angle(0.694, 0, 0) },
 		["ValveBiped.Bip01_R_UpperArm"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0, 0), angle = Angle(-5.382, 0, 0) },
@@ -75,11 +74,9 @@ function SWEP:InitializeClientsideModels()
 		["att"] = { type = "Model", model = "models/props_canal/mattpipe.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "body", pos = Vector(-0.125, -0.064, 1.406), angle = Angle(0, -67.95, -180), size = Vector(0.375, 0.375, 0.375), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
 		["body"] = { type = "Model", model = "models/props_junk/propane_tank001a.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.017, 2.075, -1.231), angle = Angle(-10.108, -98.139, -152.969), size = Vector(0.331, 0.331, 0.28), color = Color(125, 125, 125, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
 	}
-	
 end
 
 function SWEP:OnDeploy()
-
 	self.Weapon:SendWeaponAnim( ACT_VM_DRAW )
 	
 	if SERVER then
@@ -94,26 +91,33 @@ function SWEP:CanPrimaryAttack()
 	end
 	return true
 end
+
+--Precache sounds
 for i=1,6 do
 	util.PrecacheSound("ambient/energy/spark"..i..".wav")
 end
+
 function SWEP:PrimaryAttack()
-	if self.Owner.KnockedDown or self.Owner.IsHolding and self.Owner:IsHolding() then return end
-	
-	if not self:CanPrimaryAttack() then return end
-	-- if self.TempAng ~= self.ActiveAngle then return end
-	
-	self.Weapon:SetNextPrimaryFire ( CurTime() + 0.12 )	
+	if not self:CanPrimaryAttack() then
+		return
+	end
+
+	if self.Owner.KnockedDown or self.Owner.IsHolding and self.Owner:IsHolding() then
+		return
+	end
+		
+	self.Weapon:SetNextPrimaryFire(CurTime() + 0.12)	
+
 	if SERVER then
-	local tr = self.Owner:TraceLine( 54, MASK_SHOT, team.GetPlayers(TEAM_HUMAN) )
+		local tr = self.Owner:TraceLine(54, MASK_SHOT, team.GetPlayers(TEAM_HUMAN))
 	
-	local trent = tr.Entity
-	if not IsEntityValid ( trent ) then return end
+		local trent = tr.Entity
+		if not IsEntityValid ( trent ) then
+			return
+		end
 	
 		if tr.Hit and not tr.HitWorld then
-		
 			if trent.Nails and #trent.Nails > 0 then
-				
 				local griefedprop = false
 				
 				if trent._LastAttackerIsHuman then
@@ -121,39 +125,34 @@ function SWEP:PrimaryAttack()
 				end
 				
 				for i=1, #trent.Nails do
-				
 					local nail = trent.Nails[i]
 					
 					if nail and nail:IsValid() then
-						
 						if nail:GetNailHealth() < nail:GetDTInt(1) then -- nail:GetNWInt("MaxNailHealth")
-						
 							self:TakePrimaryAmmo (1)
 							
 							if not griefedprop then
-							
 								self.Owner._TorchScore = self.Owner._TorchScore + 1
 								
 								if self.Owner._TorchScore == 55 then
-									skillpoints.AddSkillPoints(self.Owner,5)
+									skillpoints.AddSkillPoints(self.Owner, 15)
 									nail:FloatingTextEffect( 5, self.Owner )
 									self.Owner:AddXP(3)
 									self.Owner._TorchScore = 0
 								end
-							
 							end
 							
 							--nail.Heal = math.Clamp(nail.Heal+2,1,nail:GetDTInt(1))
-							nail:SetNailHealth(math.Clamp(nail:GetNailHealth()+2,1,nail:GetDTInt(1)))
+							nail:SetNailHealth(math.Clamp(nail:GetNailHealth()+4,1,nail:GetDTInt(1)))
 							
 							local pos = tr.HitPos
 							local norm = tr.HitNormal-- (tr.HitPos - self.Owner:GetShootPos()):Normalize():Angle()
 							
 							local eff = EffectData()
-								eff:SetOrigin( pos )
-								eff:SetNormal( norm )
-								eff:SetScale( math.Rand(0.9,1.2) )
-								eff:SetMagnitude( math.random(10,40) )
+							eff:SetOrigin( pos )
+							eff:SetNormal( norm )
+							eff:SetScale( math.Rand(0.9,1.2) )
+							eff:SetMagnitude( math.random(10,40) )
 							util.Effect( "StunstickImpact", eff, true, true )
 							
 							self.Owner:EmitSound("ambient/energy/spark"..math.random(1,6)..".wav",math.random(86,110),math.random(86,110))
@@ -162,23 +161,21 @@ function SWEP:PrimaryAttack()
 							
 							-- BREAKER!
 							break
-						
 						end
 					end
 				end
-			
 			end
 			
 			--turret
 			if trent:GetClass() == "zs_turret" then
 				if trent:GetDTInt(1) < trent.MaxHealth then
 				
-					self:TakePrimaryAmmo (1)
+					self:TakePrimaryAmmo(1)
 					
 					self.Owner._TorchScore = self.Owner._TorchScore + 1
 								
 					if self.Owner._TorchScore == 55 then
-						skillpoints.AddSkillPoints(self.Owner,5)
+						skillpoints.AddSkillPoints(self.Owner, 15)
 						trent:FloatingTextEffect( 5, self.Owner )
 						self.Owner:AddXP(3)
 						self.Owner._TorchScore = 0
@@ -190,31 +187,27 @@ function SWEP:PrimaryAttack()
 					local norm = tr.HitNormal-- (tr.HitPos - self.Owner:GetShootPos()):Normalize():Angle()
 							
 					local eff = EffectData()
-					eff:SetOrigin( pos )
-					eff:SetNormal( norm )
-					eff:SetScale( math.Rand(0.9,1.2) )
-					eff:SetMagnitude( math.random(10,40) )
-					util.Effect( "StunstickImpact", eff, true, true )
+					eff:SetOrigin(pos)
+					eff:SetNormal(norm)
+					eff:SetScale(math.Rand(0.9,1.2))
+					eff:SetMagnitude(math.random(10,40))
+					util.Effect("StunstickImpact", eff, true, true)
 							
 					self.Owner:EmitSound("ambient/energy/spark"..math.random(1,6)..".wav",math.random(86,110),math.random(86,110))
 							
 					util.Decal("FadingScorch",tr.HitPos+tr.HitNormal,tr.HitPos-tr.HitNormal)
-					
-					
 				end
 			end
 			
 			if trent:IsPlayer() and trent:IsZombie() and trent:Alive()then
-				self:TakePrimaryAmmo (1)
+				self:TakePrimaryAmmo(1)
 				
 				trent:TakeDamage(2,self.Owner,self)
 				
 				if math.random(3) == 3 then
 					trent:EmitSound("player/pl_burnpain"..math.random(1,3)..".wav")
 				end
-				
 			end
-		
 		end
 	end
 	
@@ -230,19 +223,15 @@ end
 
 -- make everything easy
 local function ApproachAngle(ang,to,time)
-	
 	ang.p = math.Approach(ang.p, to.p, time)
 	ang.y = math.Approach(ang.y, to.y, time)
 	ang.r = math.Approach(ang.r, to.r, time)
-	
 end
 
 SWEP.fired = false
 SWEP.lastfire = 0
 SWEP.rechargetimer = 0
 function SWEP:Think()
-
-	
 	self.AppTo = self.IdleAngle
 		
 	if self.Owner:KeyDown(IN_ATTACK) and self:CanPrimaryAttack() then
@@ -281,14 +270,16 @@ function SWEP:Think()
 			end
 		end
 	end
-
 end
 
 if CLIENT then
-	
 	function SWEP:DrawHUD()
-		if not self.Owner:Alive() then return end
-		if ENDROUND then return end
+		if not self.Owner:Alive() then
+			return
+		end
+		if ENDROUND then
+			return
+		end
 		MeleeWeaponDrawHUD()
 		surface.SetFont("ArialBoldTen")
 	
@@ -299,7 +290,5 @@ if CLIENT then
 				end
 			end
 		end
-	
 	end
-
 end
