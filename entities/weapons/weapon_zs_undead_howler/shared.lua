4ae05815edda114203304b246f83105404a2fa3e
Jarno <2751672+JarnoVgr@users.noreply.github.com>
2013-06-16 09:56:16 +0000

    - Added viewmodel for Howler - Reduced damage done by Howler (33% less)

--- entities/weapons/weapon_zs_undead_howler/shared.lua
+++ entities/weapons/weapon_zs_undead_howler/shared.lua
@@ -1,7 +1,7 @@
 -- © Limetric Studios ( www.limetricstudios.com ) -- All rights reserved.
 -- See LICENSE.txt for license information
 
-if SERVER then AddCSLuaFile ( "shared.lua" ) end
+if SERVER then AddCSLuaFile ("shared.lua") end
 
 
 local math = math
@@ -15,7 +15,7 @@ if SERVER then
 	local umsg = umsg
 end
 
-
+SWEP.Base = "weapon_zs_zombie"
 SWEP.Author = "Deluvas"
 SWEP.Contact = ""
 SWEP.Purpose = ""
@@ -24,11 +24,31 @@ SWEP.Instructions = ""
 SWEP.PrintName = "Howler"
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = false
-SWEP.ViewModelFOV = 50
-SWEP.ViewModelFlip = false
-SWEP.CSMuzzleFlashes = false
 
-SWEP.ViewModel = Model ( "models/weapons/v_pza.mdl" )
+if CLIENT then
+	SWEP.ViewModelFOV = 70
+	SWEP.ViewModelFlip = false
+	SWEP.CSMuzzleFlashes = false
+	SWEP.ShowViewModel = false
+	SWEP.ShowWorldModel = false
+
+	SWEP.ViewModelBoneMods = {
+		-- ["ValveBiped.Bip01_L_Finger02"] = { scale = Vector(1.4, 1.4, 1.4), pos = Vector(0, 0, 0), angle = Angle(-10.202, 19.533, 0) },
+		["ValveBiped.Bip01_R_Forearm"] = { scale = Vector(1.2, 1.2, 1.2), pos = Vector(0, 0, 0), angle = Angle(0, -7.493, -45.569) },
+		["ValveBiped.Bip01_R_Hand"] = { scale = Vector(1.4, 1.4, 1.4), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["ValveBiped.Bip01_L_Hand"] = { scale = Vector(1.4, 1.4, 1.4), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["ValveBiped.Bip01_L_UpperArm"] = { scale = Vector(1, 1, 1), pos = Vector(5.802, 1.06, 0.335), angle = Angle(0, 0, 0) },
+		["ValveBiped.Bip01_R_Finger02"] = { scale = Vector(1.4, 1.4, 1.4), pos = Vector(0, 0, 0), angle = Angle(52.678, 0, 0) },
+		["ValveBiped.Bip01_R_Finger01"] = { scale = Vector(1.4, 1.4, 1.4), pos = Vector(0, 0, 0), angle = Angle(45.873, -0.348, 0) },
+		["ValveBiped.Bip01_L_Finger01"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0, 0), angle = Angle(-59.774, -9.223, 18.572) },
+		["ValveBiped.Bip01_L_Forearm"] = { scale = Vector(1.2, 1.2, 1.2), pos = Vector(0, 0, 0), angle = Angle(10.701, -7.301, 42.666) },
+		["ValveBiped.Bip01_L_Finger0"] = { scale = Vector(1.4, 1.4, 1.4), pos = Vector(0, 0, 0), angle = Angle(0, 9.659, 6.218) },
+		["ValveBiped.Bip01_R_Finger0"] = { scale = Vector(1.4, 1.4, 1.4), pos = Vector(0, 0, 0), angle = Angle(-6.42, 28.499, 7.317) }
+	}
+end
+
+
+SWEP.ViewModel = Model ( "models/Weapons/v_zombiearms.mdl" )
 SWEP.WorldModel = Model ( "models/weapons/w_knife_t.mdl" )
 
 SWEP.Spawnable = true
@@ -53,11 +73,15 @@ SWEP.ProtectionDistance = 220
 SWEP.PropTKDistance = 450
 SWEP.DistanceCheck = 450
 
-function SWEP:Deploy()
-	if SERVER then
+SWEP.SwapAnims = false
+SWEP.AttackAnimations = { "attackD", "attackE", "attackF" }
+
+function SWEP:OnDeploy()
+	self.Owner.ZomAnim = math.random(1, 3)
+	--[[if SERVER then
 		self.Owner:DrawViewModel( false )
 		self.Owner:DrawWorldModel( false )
-	end
+	end]]
 end
 
 function SWEP:Precache()
@@ -80,19 +104,24 @@ function SWEP:Precache()
 		util.PrecacheSound(snd)
 	end
 	
-	
-end
+end	
 
 SWEP.NextSwing = 0
 function SWEP:DoAttack( bPull ) 
-	if CurTime() < self.NextSwing then return end
+	if CurTime() < self.NextSwing then
+		return
+	end
 	
 	-- Get owner
 	local mOwner = self.Owner
-	if not ValidEntity ( mOwner ) then return end
+	if not ValidEntity ( mOwner ) then
+		return
+	end
 	
 	-- Cannot scream while in air
-	if not mOwner:OnGround() then return end
+	if not mOwner:OnGround() then
+		return
+	end
 	
 	-- Cooldown
 	self.NextSwing = CurTime() + self.Primary.Delay
@@ -121,8 +150,10 @@ function SWEP:DoAttack( bPull )
 		mOwner:EmitSound( table.Random ( ZombieClasses[ mOwner:GetZombieClass() ].AttackSounds ), 100, math.random ( 95,135 ) )
 	end
 	
-	-- Just server
-	if CLIENT then return end
+	-- Just server from here
+	if CLIENT then
+		return
+	end
 	
 	-- Get nearby howlers
 	local iHowlers = #GetHowlers( mOwner:GetPos(), 450 ) or 1
@@ -146,10 +177,10 @@ function SWEP:DoAttack( bPull )
 						local iFuckIntensity = math.Clamp ( 4.5 - ( ( fDistance / iDistance ) * 10 ), 1, 2 )
 						-- if v:GetHumanClass() == 3 then iFuckIntensity = iFuckIntensity * math.Rand(1.8,2.3) end
 						-- Event
-						GAMEMODE:OnPlayerHowlered ( v, iFuckIntensity )
+						GAMEMODE:OnPlayerHowlered(v, iFuckIntensity)
 											
 						-- Inflict damage
-						local fDamage = math.Round ( math.Clamp ( ( 22 - ( fDistance / 10 ) ) / iHowlers, 0, 5 ) * 3 )
+						local fDamage = math.Round ( math.Clamp ( ( 22 - ( fDistance / 10 ) ) / iHowlers, 0, 5 ) * 2 )
 						-- if v:GetHumanClass() == 3 then fDamage = fDamage * math.Rand(1.8,2.3) end
 						if fDamage > 0 then v:TakeDamage ( fDamage, self.Owner, self ) end
 					end
@@ -237,6 +268,8 @@ end
 
 function SWEP:Think()
 	self:CheckScream()
+
+	return self.BaseClass.Think(self)
 end
 
 function SWEP:CheckScream()
@@ -264,11 +297,11 @@ function SWEP:IsScreaming()
 end
 
 function SWEP:PrimaryAttack()
-	self:DoAttack( true )
+	self:DoAttack(true)
 end
 
 function SWEP:SecondaryAttack()
-	self:DoAttack( false )
+	self:DoAttack(false)
 end
 
 function SWEP:Move(mv)
@@ -322,7 +355,9 @@ local function howlerDoProtection()
 	-- Wraith scream
 	WraithScream()
 end
-if CLIENT then usermessage.Hook( "howlerDoProtection", howlerDoProtection ) end
+if CLIENT then
+	usermessage.Hook( "howlerDoProtection", howlerDoProtection )
+end
 
 function SWEP:Reload()
 	return false
@@ -337,7 +372,104 @@ if SERVER then
 end
 
 if CLIENT then
-	function SWEP:DrawHUD() GAMEMODE:DrawZombieCrosshair ( self.Owner, self.DistanceCheck ) end
+	function SWEP:DrawHUD()
+		GAMEMODE:DrawZombieCrosshair ( self.Owner, self.DistanceCheck )
+	end
+end
+
+function SWEP:_OnRemove()
+	if CLIENT then
+		self:RemoveArms()
+	end
+end
+
+function SWEP:OnInitialize()
+	if CLIENT then
+		self:MakeArms()
+	end
 end
 
  
+
+local lerp = 0
+function SWEP:GetViewModelPosition(pos, ang)
+	lerp = math.Approach(lerp, 0, FrameTime() * ((lerp + 1) ^ 3))
+	ang:RotateAroundAxis(ang:Right(), 64 * lerp)
+	if lerp > 0 then
+		pos = pos + -8 * lerp * ang:Up() + -12 * lerp * ang:Forward()
+	end
+	return pos, ang
+	--return self.BaseClass.GetViewModelPosition(self, pos, ang)
+end
+
+if CLIENT then
+
+function SWEP:OnViewModelDrawn()
+
+	local vm = self.Owner:GetViewModel();
+	if IsValid(self.Arms) and IsValid(vm) then
+		
+		if self.Arms:GetModel() ~= self.Owner:GetModel() and self.Owner:GetModel() ~= "models/zombie/fast.mdl" then
+			self.Arms:SetModel(self.Owner:GetModel())
+		end
+			
+		render.SetBlend(1) 
+			self.Arms:SetParent(vm)
+			
+			self.Arms:AddEffects(bit.bor(EF_BONEMERGE , EF_BONEMERGE_FASTCULL , EF_PARENT_ANIMATES))
+			
+			for b, tbl in pairs(PlayerModelBones) do
+				if tbl.ScaleDown then
+					local bone = self.Arms:LookupBone(b)
+					if bone and self.Arms:GetManipulateBoneScale(bone) == Vector(1,1,1) then
+						self.Arms:ManipulateBoneScale( bone, Vector(0.00001, 0.00001, 0.00001) )
+					end
+				else
+					if not tbl.Arm then
+						local bone = self.Arms:LookupBone(b)
+						if bone and self.Arms:GetManipulateBoneScale(bone) == Vector(1,1,1) then
+							self.Arms:ManipulateBoneScale( bone, Vector(1.5, 1.5, 1.5) )
+						end
+					end
+				end
+			end
+			
+			self.Arms:SetRenderOrigin( vm:GetPos()-vm:GetAngles():Forward()*20-vm:GetAngles():Up()*60 )-- self.Arms[1]:SetRenderOrigin( EyePos() )
+			self.Arms:SetRenderAngles( vm:GetAngles() )
+			
+			self.Arms:SetupBones()	
+			self.Arms:DrawModel()
+			
+			self.Arms:SetRenderOrigin()
+			self.Arms:SetRenderAngles()
+			
+		render.SetBlend(1)
+	end
+	
+end
+
+function SWEP:MakeArms()
+
+	self.Arms = ClientsideModel("models/player/group01/male_04.mdl", RENDER_GROUP_VIEW_MODEL_OPAQUE) 
+	
+	if (ValidEntity(self.Arms)) and (ValidEntity(self)) then 
+		self.Arms:SetPos(self:GetPos())
+		self.Arms:SetAngles(self:GetAngles())
+		self.Arms:SetParent(self) 
+		self.Arms:SetupBones()
+		-- self.Arms:AddEffects(bit.bor(EF_BONEMERGE , EF_BONEMERGE_FASTCULL , EF_PARENT_ANIMATES))
+		self.Arms:SetNoDraw(true) 
+	else
+		self.Arms = nil
+	end
+	
+end
+
+function SWEP:RemoveArms()
+	if (ValidEntity(self.Arms)) then
+		self.Arms:Remove()
+	end
+		self.Arms = nil
+end
+
+end
