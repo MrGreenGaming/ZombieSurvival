1823c69dbf23a9f58c992711644db31302c4e035
Jarno <2751672+JarnoVgr@users.noreply.github.com>
2013-11-12 11:29:43 +0000

    Experimental new feature: Random prop spawning throughout map. Currently testing with The Fortress.

--- gamemode/shared/maps/zs_fortress_mod.lua
+++ gamemode/shared/maps/zs_fortress_mod.lua
@@ -31,7 +31,23 @@ end]]
 --Make map darker
 --timer.Simple(1,function() engine.LightStyle(0,"b") end)
 if SERVER then
-	hook.Add("InitPostEntity", "MapC_Init", function()
+	--Props to pick randomly from
+	local props = {
+		"models/props_junk/PlasticCrate01a.mdl",
+		"models/props_borealis/bluebarrel001.mdl",
+		"models/props_c17/chair02a.mdl",
+		"models/props_junk/wood_crate001a_damaged.mdl",
+		"models/props_junk/wood_crate001a.mdl",
+		"models/props_junk/wood_crate002a.mdl",
+		"models/props_junk/MetalBucket01a.mdl",
+		"models/props_vehicles/tire001b_truck.mdl",
+		"models/props_vehicles/tire001c_car.mdl",
+		"models/props_vehicles/apc_tire001.mdl",
+		"models/props_junk/TrafficCone001a.mdl"
+	}
+
+	local function MapCInitEntities()
+		--Paint skybox
 		env_skypaint = ents.Create("env_skypaint")
 		env_skypaint:Spawn()
 		env_skypaint:Activate()
@@ -42,10 +58,26 @@ if SERVER then
 		env_skypaint:SetSunColor(Vector(0,0,0))
 		env_skypaint:SetStarScale(1.1)
 	
+		--Set skybox
 		game.ConsoleCommand("sv_skyname painted\n")
 
+		--Set lighting style (make it more dark)
 		engine.LightStyle(0,"b")
-	end)
+
+		--Spawn random props througout map
+		local maxProps = math.random(60,80)
+		for i=1,maxProps do
+			local e = ents.Create("prop_physics")
+			--X: -1300 to 1200
+			e:SetPos(Vector(math.random(-1000,1000), math.random(-1425,702), 800))
+			e:SetModel(props[math.random(1,#props)])
+			e:SetAngles(Angle(math.random(0,360),math.random(0,360),math.random(0,360)))
+			e:Spawn()
+		end
+		Debug("[MAPCODER] Spawned ".. maxProps .." props")
+	end
+
+	hook.Add("InitPostEntity", "MapC_InitEntities", MapCInitEntities)
 end
 
 if CLIENT then
@@ -84,6 +116,14 @@ if CLIENT then
 		hook.Add( "SetupWorldFog","MapC_AddNightFog", MapCAddNightFog )
 		hook.Add( "SetupSkyboxFog","MapC_AddNightFogSkybox", MapCAddNightFogSkybox )	
 
-		print("[MAPC] ADDED FOG")
+		print("[MAPCODER] Created fog")
 	end)
+--[[
+Model: models/player/mossman.mdl
+Position: 104.405632 -673.995178 192.031250
+Team: Survivors
+***END DEBUG***
+***DEBUG***
+Name: Ywa
+S]]
 end
