8a0cd517563efa30a78202ad4d448c112417152a
duby72 <14262325+Dubyx@users.noreply.github.com>
2014-05-01 20:26:15 +0000

    -Turret remote is now in tool slot 2.

--- gamemode/shared/zs_options_shared.lua
+++ gamemode/shared/zs_options_shared.lua
@@ -225,7 +225,7 @@ GM.HumanWeapons = {
 	["weapon_zs_tools_hammer"]  = { Name = "Nailing Hammer", DPS = 23, Infliction = 0, Type = "tool1", NoRetro = true },
 	["weapon_zs_medkit"]  = { Name = "Medkit", DPS = 8, Infliction = 0, Type = "tool1", NoRetro = true },
 	["weapon_zs_tools_supplies"] = { Name = "Mobile Supplies", DPS = 0, Infliction = 0, Type = "tool1", NoRetro = true },
-	["weapon_zs_tools_remote"] = { Name = "Remote Controller", DPS = 0, Infliction = 0, Type = "tool1" },
+	["weapon_zs_tools_remote"] = { Name = "Remote Controller", DPS = 0, Infliction = 0, Type = "tool2" },
 	["weapon_zs_tools_torch"] = { Name = "Torch", DPS = 0, Infliction = 0, Type = "tool2", NoRetro = true },
 	
 	--Tool2
