18651f73d0278caddcf8c4526e647023210baf9d
duby72 <14262325+Dubyx@users.noreply.github.com>
2014-05-29 22:15:54 +0000

    -Removed horse health for debugging. 'Weird glitch occurred.'

--- gamemode/server/entitytakedamage/sv_event_onplayerdamage.lua
+++ gamemode/server/entitytakedamage/sv_event_onplayerdamage.lua
@@ -54,28 +54,28 @@ local function OnPlayerDamage( pl, attacker, inflictor, dmginfo )
 	--Duby:Hey you know I love to code with silly stuff. Well I gave horse health a revamp!! :O 
 	--Duby: Please do not fuck with the medkit or anything as you will literally fuck the balance of the server!! >:(
 		
-	local HealthRegen = {}
-HealthRegen.Amount = 1
-HealthRegen.GiveDelay = 1
-HealthRegen.MaxRegen= 30
+--	local HealthRegen = {}
+--HealthRegen.Amount = 1
+--HealthRegen.GiveDelay = 1
+--HealthRegen.MaxRegen= 30
 
-hook.Add( "Think", "RegenHealth", function()
+--hook.Add( "Think", "RegenHealth", function()
 
 
---if pl:GetPerk() == "_horsehealth" then
-if pl:HasBought("horse") then
-	for k,v in pairs( player.GetAll() ) do
-		if v:Alive()  and v:Health() < HealthRegen.MaxRegen and ( !v.lastregen or v.lastregen < CurTime() - HealthRegen.GiveDelay ) then
+
+--if pl:HasBought("horse") then
+	--for k,v in pairs( player.GetAll() ) do
+	--	if v:Alive()  and v:Health() < HealthRegen.MaxRegen and ( !v.lastregen or v.lastregen < CurTime() - HealthRegen.GiveDelay ) then
 		
-			v.lastregen = CurTime()
-			v:SetHealth( v:Health() + HealthRegen.Amount )
+		--	v.lastregen = CurTime()
+		--	v:SetHealth( v:Health() + HealthRegen.Amount )
 			
-		end
-	end
+		--end
+	--end
 	
-end
+--end
 	
-end )
+--end )
 	
 	
 end
