60296d7bd69c56061e07d6bebce07545633a0683
duby72 <14262325+Dubyx@users.noreply.github.com>
2014-06-07 14:39:58 +0000

    -Fix for the medkit.

--- entities/weapons/weapon_zs_medkit/shared.lua
+++ entities/weapons/weapon_zs_medkit/shared.lua
@@ -1,306 +1,301 @@
-AddCSLuaFile()
+if SERVER then
+	AddCSLuaFile("shared.lua")
+end
 
 if CLIENT then
-	SWEP.ViewModelFOV = 54
-	SWEP.PrintName = "MedKit"
+	SWEP.ViewModelFOV = 65
+	SWEP.BobScale = 2
+	SWEP.SwayScale = 1.5
+	SWEP.PrintName = "Medkit"
 
 	SWEP.Slot = 4
 	SWEP.SlotPos = 0
 	
 	SWEP.ViewModelFlip = false
+		
+	-- SWEP.IgnoreBonemerge = true
+	-- SWEP.UseHL2Bonemerge = true
+	--SWEP.ShowViewModel = true
+	--SWEP.AlwaysShowViewModel = true
+	--SWEP.ShowWorldModel = false
+
 	SWEP.ShowViewModel = true
-	SWEP.ShowWorldModel = true
+	SWEP.ShowWorldModel = false
+	SWEP.IgnoreBonemerge = true
 	
-	--Hide HUD. We have a custom display
+	SWEP.IgnoreThumbs = true
+	
 	SWEP.NoHUD = true
 		
 	killicon.AddFont( "weapon_zs_medkit", "CSKillIcons", "F", Color(255, 255, 255, 255 ) )
 	
 end
 
-SWEP.ViewModel = Model("models/weapons/c_medkit.mdl")
-SWEP.UseHands = true
-SWEP.WorldModel = Model("models/weapons/w_medkit.mdl")
+SWEP.WorldModel = "models/Weapons/w_package.mdl"
+SWEP.ViewModel = "models/weapons/v_c4.mdl"--models/weapons/v_hands.mdlmodels/weapons/v_healthkit.mdl
 
-SWEP.Base = "weapon_zs_base_dummy"
+SWEP.Base				= "weapon_zs_base_dummy"
 
-SWEP.Primary.Delay = 0.7
-SWEP.Primary.Automatic	= true
-SWEP.Primary.Heal = 10
-SWEP.Primary.ClipSize = 80
-SWEP.Primary.DefaultClipSize = 80
-SWEP.Primary.UpgradedClipSize = 150
-SWEP.Primary.DefaultClip = 80
-SWEP.Primary.Ammo = "SniperRound"
+SWEP.Primary.Delay = 0.01
 
+SWEP.Primary.Heal = 15
+SWEP.Primary.HealDelay = 10
 
-SWEP.Secondary.Delay = 0.7
-SWEP.Secondary.Automatic	= true
+SWEP.Primary.ClipSize = 30
+SWEP.Primary.DefaultClip = 50
+SWEP.Primary.Ammo = "Battery"
+
+SWEP.Secondary.Delay = 0.01
+
 SWEP.Secondary.Heal = 10
-SWEP.Secondary.ClipSize = 80
-SWEP.Secondary.DefaultClipSize = 80
-SWEP.Secondary.UpgradedClipSize = 150
-SWEP.Secondary.DefaultClip = 80
-SWEP.Secondary.Ammo = "SniperRound"
+SWEP.Secondary.HealDelay = 20
 
-SWEP.Secondary.Automatic	= true
-SWEP.WalkSpeed = 190
+SWEP.Secondary.ClipSize = 1
+SWEP.Secondary.DefaultClip = 1
+SWEP.Secondary.Ammo = "CombineCannon"
+
+SWEP.WalkSpeed = 185
 
 SWEP.NoMagazine = true
 
 SWEP.HoldType = "slam"
 
---SWEP.NoDeployDelay = true
+SWEP.NoDeployDelay = true
 
-function SWEP:OnInitialize()
-	if CLIENT then
-		return
-	end
+function SWEP:InitializeClientsideModels()
 	
-	self.Weapon.FirstSpawn = true
+	self.ViewModelBoneMods = {
+		["v_weapon.button2"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.c4"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.button5"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.button0"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.Left_Arm"] = { scale = Vector(1, 1, 1), pos = Vector(-0.486, 0.456, 0.785), angle = Angle(0, 0.638, 0) },
+		["v_weapon.button3"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.Right_Arm"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0, 0), angle = Angle(6.224, 3.197, -1.892) },
+		["v_weapon.button6"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.button8"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.button1"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.button9"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.button4"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+		["v_weapon.button7"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) }
+	}
+
+	
+	self.VElements = {
+		["medkit"] = { type = "Model", model = "models/items/HealthKit.mdl", bone = "v_weapon.c4", rel = "", pos = Vector(-2.849, 0.319, 1.254), angle = Angle(-93, 90, 5), size = Vector(0.5, 0.5, 0.5), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	}
+	
+	self.WElements = {
+		["medkit"] = { type = "Model", model = "models/items/HealthKit.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(6.224, 5.181, -1.887), angle = Angle(40.43, 174.731, -180), size = Vector(0.6, 0.6, 0.6), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	}
+	
 end
 
+function SWEP:OnInitialize()
+	if SERVER then
+		self.Weapon.FirstSpawn = true
+	end	
+end
+
+function SWEP:Think()
+	if self.IdleAnimation and self.IdleAnimation <= CurTime() then
+		self.IdleAnimation = nil
+		--self:SendWeaponAnim(ACT_VM_IDLE)
+	end
+end
 util.PrecacheSound("items/medshot4.wav")
 util.PrecacheSound("items/medshotno1.wav")
 util.PrecacheSound("items/smallmedkit1.wav")
 function SWEP:PrimaryAttack()
+	if self:CanPrimaryAttack() then
+		local owner = self.Owner
+		local trace = self.Owner:GetEyeTrace()
+		if trace.HitPos:Distance(self.Owner:GetShootPos()) <= 80 then
+			local ent = self.Owner:GetEyeTrace().Entity
 
-localplayer = pl
-	if not self:CanPrimaryAttack() then
-		if SERVER then
-			self.Owner:EmitSound(Sound("items/medshotno1.wav"))
-		end
+		-- local ent = owner:MeleeTrace(32, 2).Entity
+			if ent:IsValid() and ent:IsPlayer() and ent:Alive() and ent:Team() == TEAM_HUMAN then
 
-		return
-	end
+				local health, maxhealth = ent:Health(), 100-- owner:GetMaxHealth()
+				if ent:GetPerk("_kevlar") then maxhealth = 110 elseif ent:GetPerk("_kevlar2") then maxhealth = 120 end
+				local multiplier = 1
+				if owner:GetPerk("_medupgr1" ) then
+					multiplier = 1.35
+				end
+				local toheal = math.min(self:GetPrimaryAmmoCount(), math.ceil(math.min(self.Primary.Heal * multiplier, maxhealth - health)))
+				local totake = math.ceil(toheal / multiplier)
+				if toheal > 0 then
+					
+					local delay = self.Primary.HealDelay
+					if owner:GetSuit() == "medicsuit" then
+						delay = math.Clamp(self.Primary.HealDelay - 3,0,self.Primary.HealDelay)
+					end
+					
+					self:SetNextCharge(CurTime() + delay)
+					owner.NextMedKitUse = self:GetNextCharge()
+					
+					if SERVER then
+						owner.HealingDone = owner.HealingDone + (toheal or 10)
+						skillpoints.AddSkillPoints(owner,toheal or 10)
+						ent:FloatingTextEffect( toheal or 10, owner )
+						owner:AddXP(toheal or 5)
+						
+						--log.PlayerOnPlayerAction( self.Owner, ent, "heal_other", {["amount"] = (toheal or 10)})
+						
+						self:TakeCombinedPrimaryAmmo(totake)
 
-	self.Weapon:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-	self.Weapon:SetNextSecondaryFire(CurTime() + self.Primary.Delay)
+						ent:SetHealth(health + toheal)
+						ent:EmitSound("items/medshot4.wav")
+						
+						if math.random(9) == 9 then
+							if VoiceSets[owner.VoiceSet] then
+								local snd = VoiceSets[owner.VoiceSet].HealSounds or {}
+								local toplay = snd[math.random(1, #snd)]
+								if toplay then
+									owner:EmitSound(toplay)
+								end
+							end
+						end
+					end
+					-- end
+					-- self:SendWeaponAnim(ACT_VM_SECONDARYATTACK)
 
+					owner:SetAnimation( PLAYER_ATTACK1 )
 
-	local owner = self.Owner
-	local trace = self.Owner:GetEyeTrace()
+					self.IdleAnimation = CurTime() + self:SequenceDuration()
 
-	--Check if not too far away
-	if trace.HitPos:Distance(self.Owner:GetShootPos()) > 80 then
-		return
-	end
-
-	--Get trace entity
-	local ent = trace.Entity
-
-	--Several checks for guy to heal
-	if not ent:IsValid() or not ent:IsPlayer() or not ent:Alive() or ent:Team() ~= TEAM_HUMAN then
-		return
-	end
-
-	--Get vars
-	local health, maxhealth = ent:Health(), 100
-
-	--Set max health higher if ent has certain perks
-	if ent:GetPerk("_kevlar") then
-		maxhealth = 110
-	elseif ent:GetPerk("_kevlar2") then
-		maxhealth = 130
-	elseif ent:GetPerk("_kevlar3") then
-		maxhealth = 105
-	end
-
-	--Medical upgrade (multiplier)
-	local multiplier = 1
-	if owner:GetPerk("_medupgr1" ) then
-		--multiplier = 1.35
-		skillpoints.AddSkillPoints( pl, 3)
-	end
-
-	if ValidEntity(self:GetOwner()) and self:GetOwner():GetSuit() == "medicsuit" then --Walk slower with the medsuit and  heal
-			Walkspeed = 150
-			Primary.DefaultClipSize = 120
-			Primary.Heal = 20	
-		end
-	
-	
-	local toheal = math.min(self:GetPrimaryAmmoCount(), math.ceil(math.min(self.Primary.Heal * multiplier, maxhealth - health)))
-	local totake = math.ceil(toheal / multiplier)
-	if toheal <= 0 then
-		return
-	end
-
-	self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
-	self.Owner:SetAnimation(PLAYER_ATTACK1)
-
-	if CLIENT then
-		return
-	end
-	
-	owner.HealingDone = owner.HealingDone + (toheal or 10)
-	skillpoints.AddSkillPoints(owner,toheal or 10)
-	ent:FloatingTextEffect( toheal or 10, owner)
-	owner:AddXP(toheal or 5)
-
-	--log.PlayerOnPlayerAction( self.Owner, ent, "heal_other", {["amount"] = (toheal or 10)})
-
-	--self:TakeCombinedPrimaryAmmo(totake)
-	self:TakePrimaryAmmo(totake)
-
-	ent:SetHealth(health + toheal)
-	ent:EmitSound(Sound("items/medshot4.wav"))
-
-	--HERE PATCH YOURSELF UP
-	if math.random(9) == 9 then
-		if VoiceSets[owner.VoiceSet] then
-			local snd = VoiceSets[owner.VoiceSet].HealSounds or {}
-			local toplay = snd[math.random(1, #snd)]
-			if toplay then
-				owner:EmitSound(toplay)
+				end
 			end
 		end
+	else
+		if SERVER then self.Owner:EmitSound("items/medshotno1.wav") end
 	end
 end
 
 function SWEP:SecondaryAttack()
-	if not self:CanPrimaryAttack() then
-		if SERVER then
-			self.Owner:EmitSound(Sound("items/medshotno1.wav"))
-		end
-
-		return
-	end
-
-	self.Weapon:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-	self.Weapon:SetNextSecondaryFire(CurTime() + self.Primary.Delay)
-
-	if CLIENT then
-		return
-	end
-
-	if ValidEntity(self:GetOwner()) and self:GetOwner():GetSuit() == "medicsuit" then --Walk slower with the medsuit and  heal
-			Walkspeed = 150
-			Heal = 20
-			DefaultClipSize = 120
-		end
-	
-	--Define vars
 	local owner = self.Owner
-	local health, maxhealth = owner:Health(), 100-- owner:GetMaxHealth()
-	
-	--Check for perks
-	if owner:GetPerk("_kevlar") then
-		maxhealth = 110
-	elseif owner:GetPerk("_kevlar2") then
-		maxhealth = 130
-	end
-	
-	--Check for medical upgrade (multiplier)
-	local multiplier = 1
-	if owner:GetPerk("_medupgr1") then
-		multiplier = 1.35
-	end
-	
-	--
-	local toheal = math.min(self:GetPrimaryAmmoCount(), math.ceil(math.min(self.Primary.Heal * multiplier, maxhealth - health)))
-	local totake = math.ceil(toheal / multiplier)
-	if toheal <= 0 then
-		return
-	end
-
-	self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
-	self.Owner:SetAnimation(PLAYER_ATTACK1)
-
-	if CLIENT then
-		return
-	end
-			
-	self:TakeCombinedPrimaryAmmo(totake)
-
-	--log.PlayerAction( self.Owner, "heal_self", {["amount"] = toheal})
-	
-	owner:SetHealth(health + toheal)
+	if self:CanPrimaryAttack() then
+		local health, maxhealth = owner:Health(), 100-- owner:GetMaxHealth()
+		if owner:GetPerk("_kevlar") then maxhealth = 110 elseif owner:GetPerk("_kevlar2") then maxhealth = 120 end
+		local multiplier = 1
+		if owner:GetPerk("_medupgr1") then
+			multiplier = 1.35
+		end
+		local toheal = math.min(self:GetPrimaryAmmoCount(), math.ceil(math.min(self.Secondary.Heal * multiplier, maxhealth - health)))
+		local totake = math.ceil(toheal / multiplier)
+		if toheal > 0 then
 		
-	owner:EmitSound(Sound("items/smallmedkit1.wav"))
+			local delay = self.Secondary.HealDelay
+			if owner:GetSuit() == "medicsuit" then
+				delay = math.Clamp(self.Secondary.HealDelay - 3,0,self.Secondary.HealDelay)
+			end
+			
+			self:SetNextCharge(CurTime() + delay)
+			owner.NextMedKitUse = self:GetNextCharge()
+			
+			self:TakeCombinedPrimaryAmmo(totake)
+			if SERVER then	
+				--log.PlayerAction( self.Owner, "heal_self", {["amount"] = toheal})
+			
+				owner:SetHealth(health + toheal)
+				
+				owner:EmitSound("items/smallmedkit1.wav")
+			end
+			-- self:SendWeaponAnim(ACT_VM_SECONDARYATTACK)
+
+			owner:SetAnimation( PLAYER_ATTACK1 )
+			self.IdleAnimation = CurTime() + self:SequenceDuration()
+		end
+	else
+		if SERVER then self.Owner:EmitSound("items/medshotno1.wav") end
+	end
+end
+	
+function SWEP:OnDeploy()
+	self.IdleAnimation = CurTime() + self:SequenceDuration()
+	--self:SendWeaponAnim(ACT_VM_IDLE)
 end
 
-SWEP.RechargeTimer = 0
-function SWEP:Think()
-	self.BaseClass.Think(self)
+function SWEP:SetNextCharge(tim)
+	self:SetDTFloat(0, tim)
+end
 
-	--Set secondary ammo in clip
-	local ammocount = self.Owner:GetAmmoCount(self.Primary.Ammo)
-	if 0 < ammocount then
-		self:SetClip1(ammocount + self:Clip1())
-		self.Owner:RemoveAmmo(ammocount, self.Primary.Ammo)
-	end
+function SWEP:GetNextCharge()
+	return self:GetDTFloat(0)
+end
+
+if CLIENT then
+	local texGradDown = surface.GetTextureID("VGUI/gradient_down")
+	function SWEP:DrawHUD()
 	
-	
-			
-	
-	if SERVER then
-		if not self.Owner:KeyDown(IN_ATTACK) and self.RechargeTimer < CurTime() and self.Weapon:Clip1() < self.Primary.ClipSize then	
-			--Give 5
-			self.Weapon:SetClip1(math.min(self.Primary.ClipSize, self.Weapon:Clip1() + 10))
-			
-			--Next recharge
-			self.RechargeTimer = CurTime() + 10
+		local wid, hei = ScaleW(150), ScaleH(33)
+		local space = 12+ScaleW(7)
+		local x, y = ScrW() - wid - 12, ScrH() - ScaleH(73) - 12
+		y = y + ScaleH(73)/2 - hei/2
+		surface.SetFont("ssNewAmmoFont13")
+		local tw, th = surface.GetTextSize("Medical Kit")
+		local texty = y + hei/2 
+		
+		surface.SetDrawColor( 0, 0, 0, 150)
+		
+		surface.DrawRect(x, y, wid, hei)
+		surface.DrawRect(x+3, y+3, wid-6, hei-6)
+
+		local timeleft = self:GetNextCharge() - CurTime()
+		if 0 < timeleft then
+			surface.SetDrawColor(255, 255, 255, 180)
+			surface.SetTexture(texGradDown)
+			surface.DrawTexturedRect(x+3, y+3, math.min(1, timeleft / math.max(self.Primary.HealDelay, self.Secondary.HealDelay)) * (wid-6), hei-6)
+		end
+
+		-- surface.SetDrawColor(255, 0, 0, 180)
+		-- surface.DrawOutlinedRect(x, y, wid, hei)
+
+		-- draw.SimpleText("Medical Kit", "ZSHUDFontSmall", x, texty, COLOR_GREEN, TEXT_ALIGN_LEFT)
+
+		local charges = self:GetPrimaryAmmoCount()
+		if charges > 0 then
+			draw.SimpleTextOutlined(charges, "ssNewAmmoFont13", x-8, texty, Color(255,255,255,255), TEXT_ALIGN_RIGHT,TEXT_ALIGN_CENTER,1,Color(0,0,0,255))
+		else
+			draw.SimpleTextOutlined(charges, "ssNewAmmoFont13", x-8, texty, COLOR_DARKRED, TEXT_ALIGN_RIGHT,TEXT_ALIGN_CENTER,1,Color(0,0,0,255))
 		end
 	end
 end
 
 function SWEP:CanPrimaryAttack()
 	local owner = self.Owner
-	if self.Owner.KnockedDown or self.Owner.IsHolding and self.Owner:IsHolding() then
-		return false
-	end
+	if self.Owner.KnockedDown or self.Owner.IsHolding and self.Owner:IsHolding() then return false end
 
 	if self:GetPrimaryAmmoCount() <= 0 then
+		self:SetNextCharge(CurTime() + 0.75)
+		owner.NextMedKitUse = self:GetNextCharge()
+		--self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
+		--owner.NextMedKitUse = self:GetNextPrimaryFire()
 		return false
 	end
-
-	return true
-end
-
-function SWEP:Deploy()
-	--Medical Pack perk
-	if self.Owner:GetPerk("_medupgr2") then
-		self.Weapon.Primary.ClipSize = self.Weapon.Primary.UpgradedClipSize
-	else
-		self.Weapon.Primary.ClipSize = self.Weapon.Primary.DefaultClipSize
-	end
-
-	--Adjust current amount if max is lower
-	if self.Weapon:Clip1() > self.Weapon.Primary.ClipSize then
-		self.Weapon:SetClip1(self.Weapon.Primary.ClipSize)
-	end
-end
-
-function SWEP:Equip(NewOwner)
-	if CLIENT then
-		return
-	end
 	
-	--Call this function to update weapon slot and others
-	gamemode.Call("OnWeaponEquip", NewOwner, self)
+	return --[=[self:GetNextCharge() <= CurTime() and]=] (owner.NextMedKitUse or 0) <= CurTime()
 end
 
-
-if CLIENT then
-	local texGradDown = surface.GetTextureID("VGUI/gradient_down")
-	function SWEP:DrawHUD()
-		local wid, hei = ScaleW(150), ScaleH(33)
-		local x, y = ScrW() - wid - 12, ScrH() - ScaleH(73) - 12
-		y = y + ScaleH(73)/2 - hei/2
-		
-		surface.SetDrawColor(0, 0, 0, 150)
-
-		--Draw background box
-		surface.DrawRect(x, y, wid, hei)
-		surface.DrawRect(x+3, y+3, wid-6, hei-6)
-
-		--Draw filled box based on current clipsize
-		local clipFillSizePercentage = math.Clamp(self.Weapon:Clip1() / self.Primary.ClipSize, 0, 1)
-		if clipFillSizePercentage > 0 then
-			surface.SetDrawColor(255, 255, 255, 180)
-			surface.SetTexture(texGradDown)
-			surface.DrawTexturedRect(x+3, y+3, clipFillSizePercentage * (wid-6), hei-6)
+function SWEP:Equip ( NewOwner )
+	if CLIENT then return end
+	
+	if self.Weapon.FirstSpawn then
+		self.Weapon.FirstSpawn = false
+		if NewOwner:GetPerk("_medupgr2") then
+			NewOwner:GiveAmmo( 70, self:GetPrimaryAmmoTypeString() )
 		end
-	end
+	else
+		if self.Ammunition then
+			self:TakePrimaryAmmo ( self:Clip1() - self.Ammunition )
+		end
+	
+		NewOwner:RemoveAmmo ( 1500, self:GetPrimaryAmmoTypeString() )
+		if self.Weapon.RemainingAmmunition then
+			NewOwner:GiveAmmo( self.Weapon.RemainingAmmunition or self.Primary.DefaultClip, self:GetPrimaryAmmoTypeString() )
+		end
+	end	
+	
+	-- Call this function to update weapon slot and others
+	gamemode.Call ( "OnWeaponEquip", NewOwner, self )
 end
