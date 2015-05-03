87b4be0312febe72fedd6a48b3b099a3b92b28de
Joseph Radu <11723232+JosephRadu@users.noreply.github.com>
2015-05-03 21:12:39 +0000

    - Updated musket looks.

--- entities/weapons/weapon_zs_musket/shared.lua
+++ entities/weapons/weapon_zs_musket/shared.lua
@@ -7,15 +7,32 @@ if CLIENT then
 	SWEP.SlotPos = 4
 	SWEP.ViewModelFOV = 60
 	
+	SWEP.ViewModelBoneMods = {
+		["v_weapon.scout_Parent"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) }
+	}
+
+	SWEP.IronSightsPos = Vector(-6.72, -10.947, 3.599)
+	SWEP.IronSightsAng = Vector(0, 0, 0)
+
 	SWEP.VElements = {
-	["scope"] = { type = "Model", model = "models/Items/grenadeAmmo.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(0, -3.636, -10.91), angle = Angle(180, 1.169, 0), size = Vector(0.4, 0.4, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
-	["stock2"] = { type = "Model", model = "models/props_phx/gears/bevel12.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(0, -2.597, -2), angle = Angle(0, 125.065, 0), size = Vector(0.153, 0.153, 0.153), color = Color(183, 181, 180, 255), surpresslightning = false, material = "models/items/w_grenadesheet", skin = 0, bodygroup = {} },
-	["stock2+"] = { type = "Model", model = "models/Mechanics/gears2/pinion_20t1.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(0, -2.8, 1), angle = Angle(115.713, 90, 90), size = Vector(0.1, 0.1, 0.1), color = Color(183, 181, 180, 255), surpresslightning = false, material = "models/items/w_grenadesheet", skin = 0, bodygroup = {} },
-	["scope3"] = { type = "Model", model = "models/weapons/w_eq_flashbang_thrown.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(0, -5.12, -7.7), angle = Angle(0, 129.74, 180), size = Vector(0.5, 0.5, 0.5), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
-	["scope2"] = { type = "Model", model = "models/XQM/cylinderx1.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(0, -2.401, -13), angle = Angle(90, 0, 0), size = Vector(1, 0.2, 0.14), color = Color(100, 100, 100, 255), surpresslightning = false, material = "models/items/w_grenadesheet", skin = 0, bodygroup = {} },
-	["stock"] = { type = "Model", model = "models/props_phx/construct/wood/wood_boardx1.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(-0.65, -2.597, -9.87), angle = Angle(90, 90, 90), size = Vector(0.3, 0.25, 0.3), color = Color(72, 65, 65, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
-	}	
-	
+		["Musket5+"] = { type = "Model", model = "models/props_canal/mattpipe.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(-0.593, -0.166, 2.266), angle = Angle(-26.39, -91.479, -180), size = Vector(0.606, 0.611, 0.356), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+		["Musket"] = { type = "Model", model = "models/props_phx/construct/wood/wood_boardx1.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(-0.639, -2.669, -11.393), angle = Angle(-90.265, -1.737, 1.827), size = Vector(0.398, 0.342, 0.34), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+		["Musket3"] = { type = "Model", model = "models/props_phx/gears/bevel12.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(-0.058, -2.794, -1.846), angle = Angle(-180, 0, -180), size = Vector(0.172, 0.172, 0.172), color = Color(255, 255, 255, 255), surpresslightning = false, material = "models/items/w_grenadesheet", skin = 0, bodygroup = {} },
+		["MusketBolt"] = { type = "Model", model = "models/props_c17/TrapPropeller_Lever.mdl", bone = "v_weapon.scout_Bolt", rel = "", pos = Vector(-0.949, 0.832, -2.416), angle = Angle(-180, 104.464, 4.921), size = Vector(0.358, 0.351, 0.389), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+		["Musket4"] = { type = "Model", model = "models/XQM/cylinderx1.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(0.086, -1.818, -15.693), angle = Angle(-94.935, -56.438, 42.4), size = Vector(0.56, 0.237, 0.212), color = Color(255, 255, 255, 255), surpresslightning = false, material = "models/items/w_grenadesheet", skin = 0, bodygroup = {} },
+		["Musket3+"] = { type = "Model", model = "models/Mechanics/gears2/pinion_20t1.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(-0.087, -3.056, 0.018), angle = Angle(-119.059, -94.483, 86.836), size = Vector(0.078, 0.165, 0.071), color = Color(255, 255, 255, 255), surpresslightning = false, material = "models/items/w_grenadesheet", skin = 0, bodygroup = {} },
+		["MusketMag"] = { type = "Model", model = "models/props_interiors/Radiator01a.mdl", bone = "v_weapon.scout_Clip", rel = "", pos = Vector(-0.125, -0.255, 0.319), angle = Angle(0, 0.72, 0), size = Vector(0.082, 0.032, 0.082), color = Color(255, 255, 255, 255), surpresslightning = false, material = "models/onfire", skin = 0, bodygroup = {} },
+		["Musket2"] = { type = "Model", model = "models/Items/grenadeAmmo.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(0.006, -3.369, -12.969), angle = Angle(-180, -180, 0.101), size = Vector(0.5, 0.5, 1.784), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+		["Musket5"] = { type = "Model", model = "models/props_canal/mattpipe.mdl", bone = "v_weapon.scout_Parent", rel = "", pos = Vector(0.01, -3.342, -17.098), angle = Angle(0, -91.479, -180), size = Vector(0.5, 0.335, 0.981), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	}
+
+	SWEP.WElements = {
+		["Musket4"] = { type = "Model", model = "models/XQM/cylinderx1.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(15.309, 0.221, -4.253), angle = Angle(0, 0, 0), size = Vector(0.56, 0.237, 0.212), color = Color(255, 255, 255, 255), surpresslightning = false, material = "models/items/w_grenadesheet", skin = 0, bodygroup = {} },
+		["Musket5+"] = { type = "Model", model = "models/props_canal/mattpipe.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(0.05, 0.97, -2.174), angle = Angle(93.925, 180, 2.303), size = Vector(0.815, 1.343, 0.61), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+		["Musket5"] = { type = "Model", model = "models/props_canal/mattpipe.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(19.535, 0.505, -4.665), angle = Angle(94.612, 179.673, 2.286), size = Vector(0.5, 0.419, 0.93), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+		["Musket2"] = { type = "Model", model = "models/Items/grenadeAmmo.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(14.555, 0.245, -5.502), angle = Angle(87.372, 0, 0), size = Vector(0.5, 0.708, 1.784), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+		["Musket"] = { type = "Model", model = "models/props_phx/construct/wood/wood_boardx1.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(13.029, 0.957, -4.694), angle = Angle(-3.567, -1.193, -78.295), size = Vector(0.398, 0.386, 0.476), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	}
 	SWEP.IconLetter = "n"
 	killicon.AddFont("weapon_zs_musket", "CSKillIcons", SWEP.IconLetter, Color(255, 255, 255, 255 ))
 end
