8f920fe2e2a2a8765207d5e5374d97981f7f2cc2
duby72 <14262325+Dubyx@users.noreply.github.com>
2015-05-10 12:18:35 +0000

    -Further optimizations

--- gamemode/server/sv_director_heal_human.lua
+++ gamemode/server/sv_director_heal_human.lua
@@ -7,20 +7,20 @@ timer.Create("HU-RefreshCache", 15, 0, RefreshCache)
 
 --Time in seconds after last hit or hurt to wait before healing
 --local HealTimeout = 60
-local HealTimeout = 6
-local HealTimeout2 = 2
+local HealTimeout = 2
+
 
 --Amount to heal per cycle
 --local HealAmount = 20
 local HealAmount = 1
 
 local maxheal = 40
-local maxheal2 = 40
+
 
 --Interval time in seconds to heal
 --local HealInterval = 4
-local HealInterval = 1
-local HealInterval2 = 0.25
+local HealInterval = 0.25
+
 
 local function Heal()
 	local Time = CurTime()
@@ -28,7 +28,7 @@ local function Heal()
 	
 	for i=1, #Human do		
 		local pl = Human[i]
-			if pl:GetPerk("_horse") then		
+			if pl:GetPerk("_psychopath") then		
 		if not IsValid(pl) or not pl:Alive() or not pl:Team() == TEAM_HUMAN or pl:Health() >= maxheal then
 			continue
 		end
@@ -44,36 +44,5 @@ local function Heal()
 	end	
 end
 timer.Create("HU-Heal", HealInterval, 0, Heal)
-
-local function Heal()
-	local Time = CurTime()
-		
-	
-	for i=1, #Human do
-			
-		local pl = Human[i]
-			if pl:GetPerk("_psychopath") then
-	
-	if not IsValid(pl) or not pl:Alive() or not pl:Team() == TEAM_HUMAN or pl:Health() >= maxheal2 then
-			continue
-		end
-
-		
-
-		if Time < (pl.LastHit + HealTimeout2) or Time < (pl.LastHurt + HealTimeout2) then
-			continue
-		end
-
-		pl:SetHealth(math.min(pl:Health() + HealAmount, maxheal2))
-		else	
-	return
-		
-	end
-	
-	
-	end
-	
-end
-timer.Create("HU-Heal", HealInterval2, 0, Heal)
 
 
