fbcf9b7153a51b295b91b5176e9c5a0f6e0ab5db
talascio <40523890+talascio@users.noreply.github.com>
2018-10-01 12:35:47 -0400

    Update weapon_zs_fryingpan.lua

--- entities/weapons/weapon_zs_fryingpan.lua
+++ entities/weapons/weapon_zs_fryingpan.lua
@@ -1,4 +1,4 @@
--- © Limetric Studios ( www.limetricstudios.com ) -- All rights reserved.
+-- Â© Limetric Studios ( www.limetricstudios.com ) -- All rights reserved.
 -- See LICENSE.txt for license information
 
 AddCSLuaFile()
@@ -8,20 +8,24 @@ SWEP.Base = "weapon_zs_melee_base"
 
 if CLIENT then
 	SWEP.ShowViewModel = false
-	SWEP.ShowWorldModel = true
+	SWEP.ShowWorldModel = false
 
 	killicon.AddFont( "weapon_zs_fryingpan", "ZSKillicons", "b", Color(255, 255, 255, 255 ) ) 
 end
 
 SWEP.VElements = {
-	["pan"] = { type = "Model", model = "models/weapons/w_fryingpan.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(6.282, 2.484, 0), angle = Angle(0, 180, -90), size = Vector(1, 0.8, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	["pan"] = { type = "Model", model = "models/props_c17/metalPot002a.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(4.19, 1.572, -7.736), angle = Angle(-95.058, -180, 0), size = Vector(0.901, 0.901, 0.901), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+
+SWEP.WElements = {
+	["pan"] = { type = "Model", model = "models/props_c17/metalPot002a.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.43, 1.74, -10.311), angle = Angle(-85.32, 55.214, -5.046), size = Vector(1.011, 1.011, 1.011), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
 }
 
 -- Model paths
 SWEP.Author = "Deluvas"
 SWEP.ViewModel = Model ( "models/weapons/c_crowbar.mdl" )
 SWEP.UseHands = true
-SWEP.WorldModel = Model ( "models/weapons/w_fryingpan.mdl" )
+SWEP.WorldModel = Model ( "models/weapons/w_knife_t.mdl" )
 
 -- Name
 SWEP.PrintName = "Frying Pan"
